8795b0e9c8846a0d192eb653461b16aa7731384e
paul6ix <okporp@gmail.com>
2021-04-03 17:56:16 +0100

    Added custom field to user registration

--- Users/views.py
+++ Users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.template import loader
 from django.http import HttpResponse
@@ -9,14 +8,14 @@ from .forms import RegisterForm
 # Create your views here.
 def user_registration(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}, your account was successfully created')
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
         context = {
                 'form': form
             }
